606d20e959a3d7c938508f178706d4d9b83f3c48
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-12-24 18:47:01 -0300

    cambiar estructura input file

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -9,86 +9,71 @@ import { Observable, ReplaySubject } from 'rxjs';
 })
 export class UploadFileComponent implements OnInit {
 
-  base64Output!: string;
+  base64Output!: any;
 
-  sizeFile: number = 3000000;
+  textoAdjunto:string = 'Seleccionar adjunto';
+
+  sizeFile: number = 3145728; // 3MB
+
+  arrTipos = [  // tipos de archivos permitidos
+    'image/png',  // png
+    'image/jpeg', //jpeg
+    'image/jpg',  // jpg
+    'application/pdf',  // pdf
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',  // xlsx
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // .docx
+    'application/msword', // doc
+    'application/vnd.ms-excel'  // xls
+  ];
 
   constructor() { }
 
   ngOnInit(): void {}
 
+  limpiarDatos() {
+    this.base64Output = '';
+    this.textoAdjunto = 'Seleccionar adjunto';
+  }
+
   onFileSelected( event: any ) {
     const file = event.target.files[0]; // 1 archivo
+    
+    if(event.target.files.length !== 0) {
+      this.textoAdjunto = file.name;
+    }
 
-    const arrTipos = [  // tipos de archivos permitidos
-      'image/png',  // png
-      'image/jpeg', //jpeg
-      'image/jpg',  // jpg
-      'application/pdf',  // pdf
-      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',  // xlsx
-      'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // .docx
-      'application/msword', // doc
-      'application/vnd.ms-excel'  // xls
-    ];
-
-    this.getBase64(event.target.files[0]).then( data => {
-      console.log(data);
-      if (arrTipos.indexOf(file.type) === -1) {
+    this.getBase64( file ).then( data => {
+      if (this.arrTipos.indexOf(file.type) === -1) {
         console.log('tipo de archivo no permitido');
-      } else if (file.size > 3145728) { // 3MB
+        this.limpiarDatos();
+      } else if (file.size > this.sizeFile) { // 3MB
         console.log('tamaño de archivo no permitido');
+        this.limpiarDatos();
+      } else{
+        console.log('todo bien');
+        this.base64Output = data;
       }
     });
-
-    this.base64Output = event.target.files;
-
-    // if ( event.target.files.length !== 0 || event.target.files[0] !== null ) {
-    //   if ( file.size < this.sizeFile ) { // tamaño superior a 3MB
-    //     this.convertFile( event.target.files[0] ).subscribe( ( base64: any ) => {
-    //       this.base64Output = base64; // base64
-    //       console.log('se subio bien');
-    //     });
-    //   } else {
-    //     console.log('Tamaño superior a 3MB');
-    //   }
-    // } else {
-    //   console.log('no se eligio archivo');
-    // }
   }
 
   guardar(  ) {
-    if ( this.base64Output ) {
-      // if ( event.target.files.length == 0 || event.target.files[0] == null ) {
-      console.log('archivo subido');
+    if ( this.base64Output.length !== 0 ) {
+      console.log(this.base64Output);
     } else {
       console.log('NO subido');
     }
-    // 3MB -> 3000000 Bytes 
-
-    // if (event.target.files.length != 0 || event.target.files[0] != null ) {
-     
-    // }else {
-    //    // cuando este vacío 
-    // }
-    // console.log(this.base64Output);
-  }
-
-  convertFile(file : File) : Observable<string> {
-    const result = new ReplaySubject<string>(1);
-    const reader = new FileReader();
-
-    reader.readAsBinaryString(file);
-    reader.onload = (event) => result.next(btoa(event.target!.result!.toString()));
-
-    return result;
   }
 
   getBase64(file: any) {
-    return new Promise((resolve, reject) => {
+    if ( file !== undefined) {
+      return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = () => resolve(reader.result);
         reader.onerror = error => reject(error);
-    });
+      });
+    } else {
+      return new Promise(a => {});
+    }
   }
 }
